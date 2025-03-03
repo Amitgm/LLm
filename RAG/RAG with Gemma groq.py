1388be6b607488a2a44efc5873bb55ff738ed302
amit george <amittimer@gmail.com>
2025-03-03 11:47:20 +0400

    Update RAG with Gemma groq.py

--- RAG/RAG with Gemma groq.py
+++ RAG/RAG with Gemma groq.py
@@ -11,6 +11,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
+from langchain.chains.question_answering import load_qa_chain
 from langchain_community.vectorstores import FAISS
 
 from dotenv import load_dotenv
